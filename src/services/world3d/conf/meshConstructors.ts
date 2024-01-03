b7752a1bf42ae2586e0804cf8607a0cd16ba47a9
Joonas Halinen <joonas.halinen@tuni.fi>
2024-01-03 08:50:16 +0200

    make player 1 and 2 animations separate Previously they were playing the same animations.

--- src/services/world3d/conf/meshConstructors.ts
+++ src/services/world3d/conf/meshConstructors.ts
@@ -23,8 +23,7 @@ export default async function(babylonjs: typeof BABYLON, scene: BABYLON.Scene) {
     )).meshes[0];
     directionArrowMesh.setEnabled(false);
     
-    const playerMeshImport: BABYLON.ISceneLoaderAsyncResult = (await babylonjs.SceneLoader.ImportMeshAsync(
-        null,
+    const playerMeshImport = (await babylonjs.SceneLoader.LoadAssetContainerAsync(
         "assets/models/",
         "player.glb",
         scene
@@ -39,24 +38,30 @@ export default async function(babylonjs: typeof BABYLON, scene: BABYLON.Scene) {
             return mesh;
         },
         "Player": (id: string) => {
-            const mesh = playerMeshImport.meshes[0].clone(id, null);
+            const entries = playerMeshImport.instantiateModelsToScene();
+
+            // Clone mesh.
+            const mesh = entries.rootNodes[0] as BABYLON.Mesh;
             mesh!.rotate(babylonjs.Vector3.Up(), (-1) * Math.PI / 2);
             mesh!.scaling = mesh!.scaling.scale(0.3);
             mesh!.setEnabled(true);
-            scene.addMesh(mesh!);
+
+            const animationGroupsClone = entries.animationGroups;
+
             // Label animations.
             const animationGroups = {
-                "idle": playerMeshImport.animationGroups[0], 
-                "moveForward": playerMeshImport.animationGroups[2], 
-                "moveForwardRight": playerMeshImport.animationGroups[6], 
-                "moveRight": playerMeshImport.animationGroups[8], 
-                "moveBackward": playerMeshImport.animationGroups[1], 
-                "moveBackwardRight": playerMeshImport.animationGroups[4],
-                "moveBackwardLeft": playerMeshImport.animationGroups[3], 
-                "moveLeft": playerMeshImport.animationGroups[7],
-                "moveForwardLeft": playerMeshImport.animationGroups[5],
-                "shoot": playerMeshImport.animationGroups[9]
+                "idle": animationGroupsClone[0], 
+                "moveForward": animationGroupsClone[2], 
+                "moveForwardRight": animationGroupsClone[6], 
+                "moveRight": animationGroupsClone[8], 
+                "moveBackward": animationGroupsClone[1], 
+                "moveBackwardRight": animationGroupsClone[4],
+                "moveBackwardLeft": animationGroupsClone[3], 
+                "moveLeft": animationGroupsClone[7],
+                "moveForwardLeft": animationGroupsClone[5],
+                "shoot": animationGroupsClone[9]
             } as ICharacterAnimations;
+
             // Normalize animation speeds.
             animationGroups.moveForward.speedRatio = 1.35;
             animationGroups.moveBackward.speedRatio = 1.5;
@@ -66,6 +71,7 @@ export default async function(babylonjs: typeof BABYLON, scene: BABYLON.Scene) {
             animationGroups.moveForwardRight.speedRatio = 1.2;
             animationGroups.moveBackwardLeft.speedRatio = 1.25;
             animationGroups.moveBackwardRight.speedRatio = 1.25;
+
             return [
                 mesh,
                 animationGroups
