e76d17d231d40843b0006834e6f801b62ff97758
Joonas Halinen <joonashalinen@outlook.com>
2024-09-06 23:46:36 +0300

    fix rotating camera in multiplayer also rotating it in remote game

--- src/services/world3d/conf/remoteControllerConstructors.ts
+++ src/services/world3d/conf/remoteControllerConstructors.ts
@@ -1,4 +1,5 @@
 import CreatureBody from "../../../components/objects3d/pub/creatures/CreatureBody";
+import CreatureBodyController from "../../../components/objects3d/pub/io/CreatureBodyController";
 import RemoteCreatureBodyController from "../../../components/objects3d/pub/io/RemoteCreatureBodyController";
 import { TControllerConstructors } from "./controllerConstructors";
 
@@ -6,7 +7,10 @@ export default function createRemoteControllerConstructors(
     controllerConstructors: TControllerConstructors) {
     return {
         "RemoteCreatureBodyController": (creatureBody: CreatureBody) => {
-            const controller = controllerConstructors["CreatureBodyController"](creatureBody);
+            const controller = controllerConstructors
+                ["CreatureBodyController"](creatureBody) as CreatureBodyController;
+            controller.useDiscreteCameraRotation = false;
+            
             const remoteController = new RemoteCreatureBodyController(controller);
             remoteController.setStateProperties = controller.extractStateProperties;
             return remoteController;
